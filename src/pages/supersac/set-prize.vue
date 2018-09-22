43d8f8065d373e5a3776e1813b520d9ac87a9313
zhouyan <alphazhouyan@aerfazhouyandeMacBook-Pro.local>
2018-09-22 15:42:18 +0800

    fix(提交dist):

--- src/pages/supersac/set-prize.vue
+++ src/pages/supersac/set-prize.vue
@@ -8,7 +8,7 @@
         <h2>{{teamName}}奖励设置</h2>
     </el-row>
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="230px">
-      <el-form-item :label="teamName+'总奖励:'" v-for="(teamReward,index) in sacTeamRewardList" required>
+      <el-form-item :label="teamName+'总奖励:'" v-for="(teamReward,index) in sacTeamRewardList" :key="index" required>
         <el-col :span="8">
           <el-form-item prop="coinType">
             <el-select ref="coinType" v-model="teamReward.coinName">
@@ -32,20 +32,27 @@
         <el-button type="primary" style="width: 100px" @click="addPrize(teamReward,index)" v-show="index == sacTeamRewardList.length - 1">添加</el-button>
       </el-form-item>
       <el-form-item label="最强王者奖励百分比:" prop="kingRewardRate">
-        <el-input style="width:80%" clearable v-model="ruleForm.kingRewardRate" size="small"
-                  placeholder="请输入百分比"></el-input>
+        <el-input style="width:80%" clearable v-model="ruleForm.kingRewardRate" size="small" placeholder="请输入百分比">
+          <template slot="append">%</template>
+        </el-input>
       </el-form-item>
       <el-form-item label="荣耀黄金+持续白银奖励百分比:" prop="goldSilverRate" required>
         <el-input style="width:80%" v-model="ruleForm.goldSilverRate" size="small"
-                  placeholder="请输入百分比" clearable></el-input>
+                  placeholder="请输入百分比" clearable>
+            <template slot="append">%</template>
+        </el-input>
       </el-form-item>
       <el-form-item label="倔强青铜奖励百分比:" prop="bronzeRewardRate">
         <el-input style="width:80%" clearable v-model="ruleForm.bronzeRewardRate" size="small"
-                  placeholder="请输入百分比"></el-input>
+                  placeholder="请输入百分比">
+            <template slot="append">%</template>
+        </el-input>
       </el-form-item>
       <el-form-item label="总人数奖励百分比:" prop="eachOneRate">
         <el-input style="width:80%" clearable v-model="ruleForm.eachOneRate" size="small"
-                  placeholder="请输入百分比"></el-input>
+                  placeholder="请输入百分比">
+            <template slot="append">%</template>
+        </el-input>
       </el-form-item>
       <p class="total-prize">*各奖励百分比相加等于总奖励</p>
       <el-form-item size="small" style="text-align:center; width:80%; margin-top:60px;">
@@ -82,23 +89,23 @@
         rules: {
           bronzeRewardRate: [
             { required: true, message: '请输入数额', trigger: 'blur' },
-            { validator: checkNum, message: '限额必须为数字' }
+            { validator: checkNum, message: '必须为数字' }
           ],
           goldSilverRate: [
             { required: true, message: '请输入数额', trigger: 'blur' },
-            { validator: checkNum, message: '限额必须为数字' }
+            { validator: checkNum, message: '必须为数字' }
           ],
           kingRewardRate: [
             { required: true, message: '请输入数额', trigger: 'blur' },
-            { validator: checkNum, message: '限额必须为数字' }
+            { validator: checkNum, message: '必须为数字' }
           ],
           coinType: [
             { required: true, message: '请选择币种', trigger: 'change' },
-            { message: '账数额必须为数字' }
+            { message: '必须为数字' }
           ],
           eachOneRate: [
             { required: true, message: '请输入数额', trigger: 'blur' },
-            { validator: checkNum, message: '限额必须为数字' }
+            { validator: checkNum, message: '必须为数字' }
           ],
         }
       };
@@ -108,15 +115,22 @@
       onSubmit() {
         this.$refs.ruleForm.validate((valid) => {
           if (valid) {
+            console.log(this.ruleForm.kingRewardRate + this.ruleForm.goldSilverRate + this.ruleForm.bronzeRewardRate);
+            if (Number(this.ruleForm.kingRewardRate) + Number(this.ruleForm.goldSilverRate) + Number(this.ruleForm.bronzeRewardRate) != this.ruleForm.eachOneRate) {
+              this.$notify({
+                message: '各奖励百分比相加必须等于总奖励！',
+                type: 'error'
+              });
+              return
+            }
             const ruleForm = JSON.parse(JSON.stringify(this.ruleForm))
-            ruleForm.tranInFee = (this.ruleForm.tranInFee + this.tranInFeeUnit).trim();
-            ruleForm.tranOutFee = (this.ruleForm.tranOutFee + this.tranOutFeeUnit).trim();
+            console.log('this.teamId', this.teamId);
             this.$http.post("/supernode/backmgr/team/teamRewardSeting",{
               'teamId': this.teamId,
-              'kingRewardRate': this.ruleForm.kingRewardRate,
-              'goldRewardRate': this.ruleForm.goldSilverRate,
-              'bronzeRewardRate': this.ruleForm.bronzeRewardRate,
-              'eachOneRate': this.ruleForm.eachOneRate,
+              'kingRewardRate': this.ruleForm.kingRewardRate + '%',
+              'goldRewardRate': this.ruleForm.goldSilverRate + '%',
+              'bronzeRewardRate': this.ruleForm.bronzeRewardRate + '%',
+              'eachOneRate': this.ruleForm.eachOneRate + '%',
             }).then((res) => {
               console.log(res)
               this.$message({
@@ -192,10 +206,10 @@
         }).then((res) => {
           console.log(res)
           // debugger
-          this.ruleForm = res.result;
-          this.sacTeamRewardList = res.result.sacTeamRewardCoinResultDtoList
+          this.ruleForm = JSON.parse(JSON.stringify(res.result || {}));
+          this.sacTeamRewardList = (res.result || {}).sacTeamRewardCoinResultDtoList || []
           console.log(this.ruleForm.coinType);
-          this.ruleForm.coinType = this.sacTeamRewardList[0].coinName
+          this.ruleForm.coinType = (this.sacTeamRewardList[0] || {}).coinName
           // this.$refs.coinType[0].lable = this.sacTeamRewardList[0].coinName
         })
       },

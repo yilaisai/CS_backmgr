70127a0cfb6d5ecdc38ee8dba16b6a5c30dfe222
马欢欢 <mahuanhuan@chinaxcd.com>
2018-10-18 10:50:32 +0800

    fix some bug

--- src/pages/identity/identity-modify.vue
+++ src/pages/identity/identity-modify.vue
@@ -170,11 +170,12 @@
       this.resetForm();
       this.server_path = SERVER_PATH;
       if (this.$route.params.userId) {
+        console.log(this.$route.params, 88);
         this.ruleForm.userId = this.$route.params.userId;
         this.ruleForm.realName = this.$route.params.realName;
         this.ruleForm.cardNo = this.$route.params.cardNo;
         this.ruleForm.frontUrl = this.$route.params.cardFrontUrl;
-        this.ruleForm.backUrl = this.$route.params.cardFrontUrl;
+        this.ruleForm.backUrl = this.$route.params.cardBackUrl;
         this.ruleForm.antiUrl = this.$route.params.antiMoneyUrl;
         this.details = JSON.parse(JSON.stringify(this.ruleForm));
         this.details.phone = this.$route.params.phone;

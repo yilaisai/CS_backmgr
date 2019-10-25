c73bd001858dd8bd06e182e42cdcaba8e9ece5c3
zengzuozuo <zengzuozuo@163.com>
2019-10-25 22:59:45 +0800

    num22 正式上线

--- static/js/config.js
+++ static/js/config.js
@@ -1,11 +1,11 @@
 
 var SERVER_PATH = ''
-if(window.location.href.indexOf('http://devadmin.hongmo.io') >= 0) {
-	//测试环境 
-	SERVER_PATH = 'http://47.52.110.161:7036/'
+if(window.location.href.indexOf('https://admin.num22.org') >= 0) {
+	//num22  正式环境
+	SERVER_PATH = 'https://aip.num22.org'
 }else {//开发环境
 	// SERVER_PATH = 'http://192.168.120.42:7036/'  //林威
-	SERVER_PATH = 'http://192.168.120.108:7036/'  //付岳
+	// SERVER_PATH = 'http://192.168.120.108:7036/'  //付岳
 	// SERVER_PATH = 'http://192.168.120.194:7036/'  //李海
-	// SERVER_PATH = 'http://47.52.110.161:7036/'
+	SERVER_PATH = 'http://47.52.110.161:7036/'
 }

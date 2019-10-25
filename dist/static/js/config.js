68c01b713f98043837add30df00cc2a77ca6e03f
zengzuozuo <zengzuozuo@163.com>
2019-10-26 00:29:18 +0800

    跟换接口地址

--- dist/static/js/config.js
+++ dist/static/js/config.js
@@ -2,10 +2,11 @@
 var SERVER_PATH = ''
 if(window.location.href.indexOf('https://admin.num22.org') >= 0) {
 	//num22  正式环境
-	SERVER_PATH = 'https://aip.num22.org'
+	SERVER_PATH = 'https://api.num22.org/'
 }else {//开发环境
 	// SERVER_PATH = 'http://192.168.120.42:7036/'  //林威
 	// SERVER_PATH = 'http://192.168.120.108:7036/'  //付岳
 	// SERVER_PATH = 'http://192.168.120.194:7036/'  //李海
 	SERVER_PATH = 'http://47.52.110.161:7036/'
+	// SERVER_PATH = 'https://api.num22.org/'
 }

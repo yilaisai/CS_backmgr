54c5aa59c7baa209e4b6f345607600033356ffdf
zengzuozuo <zengzuozuo@163.com>
2019-10-25 18:11:01 +0800

    接口调整

--- dist/static/js/config.js
+++ dist/static/js/config.js
@@ -9,13 +9,3 @@ if(window.location.href.indexOf('http://devadmin.hongmo.io') >= 0) {
 	// SERVER_PATH = 'http://192.168.120.194:7036/'  //李海
 	SERVER_PATH = 'http://47.52.110.161:7036/'
 }
-
-
-
-
-
-
-
-
-
-
--- src/pages/money/capitalFlow2.vue
+++ src/pages/money/capitalFlow2.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="capitalFlow-page">
 			<el-form :inline="true"  ref="filterForm" label-width="80px" :model="filterForm" size="mini">
-				<el-form-item label="订单号:">
-					<el-input placeholder="请输入单号" v-model="filterForm.userId" class="input-with-select"></el-input>
+				<el-form-item label="昵称:">
+					<el-input placeholder="请输入昵称" v-model="filterForm.nickName" class="input-with-select"></el-input>
 				</el-form-item>
 				<el-form-item label="账号:">
-					<el-input placeholder="请输入用户账号" v-model="filterForm.userId" class="input-with-select"></el-input>
+					<el-input placeholder="请输入用户账号" v-model="filterForm.name" class="input-with-select"></el-input>
 				</el-form-item>
 				<el-form-item label="币种:">
 					<el-select v-model="filterForm.coinName" >
@@ -24,28 +24,21 @@
 				</el-form-item>
 				<el-form-item><el-button type="primary" @click.native="search">搜索</el-button></el-form-item>
 			</el-form>
-			<div style="height:100%">
-				<el-table :data="listData.list" border height="100%" size="mini">
-					<el-table-column  label="时间" width="180"  align="center">
-						<div slot-scope="scope">
-							<p>{{ $fmtDate(scope.row.createTime,'full') }}</p>
-						</div>
-					</el-table-column>
-					<el-table-column prop="coinName" label="币种" align="center" ></el-table-column>
-					<el-table-column prop="phone" label="账号" align="center" ></el-table-column>
-					<el-table-column prop="nickName" label="昵称" align="center" ></el-table-column>
-					<el-table-column prop="tradeType" label="类型" width="120" align="center">
-							<template slot-scope="scope">
-									<span >{{ scope.row.optType | optType }}</span>
-							</template>
-					</el-table-column>
-					<el-table-column prop="balanceChangeAmount" label="可用资金变化" width="120" align="center"></el-table-column>
-					<el-table-column prop="balance" label="可用资金余额" width="120" align="center"></el-table-column>
-					<el-table-column label="冻结资金变化" prop="frozenChangeAmount" width="120" align="center"></el-table-column>
-					<el-table-column label="冻结资金余额" prop="frozenAmount" width="120" align="center"></el-table-column>
-				
-				</el-table>
-			</div>
+			<el-table :data="listData.list" border height="100%" size="mini">
+				<el-table-column  label="时间" width="180"  align="center">
+					<div slot-scope="scope">
+						<p>{{ scope.row.create_time }}</p>
+					</div>
+				</el-table-column>
+				<el-table-column prop="coin_name" label="币种" align="center" ></el-table-column>
+				<el-table-column prop="phone" label="账号" align="center" ></el-table-column>
+				<el-table-column prop="nick_name" label="昵称" align="center" ></el-table-column>
+				<el-table-column prop="type_name" label="类型" width="120" align="center"></el-table-column>
+				<el-table-column prop="balance_change" label="可用资金变化" width="120" align="center"></el-table-column>
+				<el-table-column prop="balance" label="可用资金余额" width="120" align="center"></el-table-column>
+				<el-table-column label="冻结资金变化" prop="frozen_amount_change" width="120" align="center"></el-table-column>
+				<el-table-column label="冻结资金余额" prop="frozen_amount" width="120" align="center"></el-table-column>
+			</el-table>
 			<div>
 				<sac-pagination v-show="listData.list.length>0"
 					@handleChange="handleCurrentChange"
@@ -99,8 +92,8 @@ export default {
 			this.filterForm.startDate = this.selectedDate && this.$fmtDate(this.selectedDate[0].getTime())+' 00:00:00';
 			this.filterForm.endDate = this.selectedDate && this.$fmtDate(this.selectedDate[1].getTime())+' 23:59:59';
 			}
-			this.$http.post('/wallet/backmgr/user/amountFlow',this.filterForm).then(res=>{
-				const { list ,total} = res.result;
+			this.$http.post('/wallet/backmgr/trade/queryAmountFlowPage',this.filterForm).then(res=>{
+				const { list ,total} = res.result.pageInfo;
 				this.listData.list = list;
 				this.listData.total = total;
 			})

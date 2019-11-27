c2e1a6d2981862b6152d534adaca303186d78e6c
zengzuozuo <zengzuozuo@163.com>
2019-11-27 10:21:50 +0800

    fixbug

--- src/pages/LegalCurrency/LegalCurrencySetting.vue
+++ src/pages/LegalCurrency/LegalCurrencySetting.vue
@@ -45,11 +45,11 @@
 		<h3>返佣设置</h3>
 		<el-tabs type="border-card">
 				<el-form label-width="160px" ref="filterForm" size="mini">
-					<el-form-item label="全局最大兑入返佣比例:">
+					<el-form-item label="全局最大卖币返佣比例:">
 					<el-input placeholder="未设置默认0.7%" v-model="rateDetaile.totalBuyRate" class="input-with-select"><template slot="append">%</template></el-input>
 					<span>设置后三级返佣比例之和不可超过该值</span>
 				</el-form-item>
-				<el-form-item label="全局最大兑出返佣比例:">
+				<el-form-item label="全局最大买币返佣比例:">
 					<el-input placeholder="未设置默认0.2%" v-model="rateDetaile.totalSaleRate" class="input-with-select"><template slot="append">%</template></el-input>
 					<span>设置后三级返佣比例之和不可超过该值</span>
 				</el-form-item>
@@ -70,13 +70,13 @@
 						<el-option label="三级反佣" :value="3"></el-option>
 					</el-select>
 				</el-form-item>
-				<el-form-item label="默认返佣兑入返佣比例:">
-					<el-input placeholder="未设置默认0.6%" v-model="rateDetaile.buyRate" class="input-with-select">
+				<el-form-item label="默认返佣卖币返佣比例:">
+					<el-input placeholder="未设置默认0.6%" v-model="rateDetaile.saleRate" class="input-with-select">
 						<template slot="append">%</template>
 					</el-input>
 				</el-form-item>
-				<el-form-item label="默认返佣兑出返佣比例:">
-					<el-input placeholder="未设置默认0.1%" v-model="rateDetaile.saleRate" class="input-with-select">
+				<el-form-item label="默认返佣买币返佣比例:">
+					<el-input placeholder="未设置默认0.1%" v-model="rateDetaile.buyRate" class="input-with-select">
 						<template slot="append">%</template>
 					</el-input>
 				</el-form-item>

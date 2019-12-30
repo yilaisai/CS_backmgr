f45686bab09fe595b338667a8569bf891cf8404a
刘镜宏 <hongwebapp@163.com>
2019-12-30 10:34:03 +0800

    dist

--- src/pages/merchant/tradingFlowDetaile.vue
+++ src/pages/merchant/tradingFlowDetaile.vue
@@ -76,9 +76,9 @@
 						<el-input :value=" '直接 0'+detaileData.coinName+', 间接 0'+detaileData.coinName  " disabled></el-input>
 					</el-form-item>
 					<el-form-item label="承兑商方折扣:" class="big">
-						<el-input class="hasBtn" :value=" '承兑商'+detaileData.makerCommission+detaileData.coinName+', 直接'+detaileData.makerFirstCommission+detaileData.coinName+', 间接'+detaileData.makerSecondaryCommission+detaileData.coinName  " disabled>
-							<el-button slot="append" @click=" discountDetaile ">查看详情</el-button>
-						</el-input>
+						<!-- <el-input class="hasBtn" :value=" '承兑商'+detaileData.makerCommission+detaileData.coinName+', 直接'+detaileData.makerFirstCommission+detaileData.coinName+', 间接'+detaileData.makerSecondaryCommission+detaileData.coinName  " disabled> -->
+							<el-button  @click=" discountDetaile ">查看详情</el-button>
+						<!-- </el-input> -->
 					</el-form-item>
 					<el-form-item label="平台盈利:">
 						<el-input

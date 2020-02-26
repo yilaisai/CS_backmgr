704f40640f1440b84cbdc74f5d7f1c9eada3b11b
刘镜宏 <hongwebapp@163.com>
2020-02-26 19:37:39 +0800

    dist

--- dist/static/js/config.js
+++ dist/static/js/config.js
@@ -12,8 +12,8 @@ if(window.location.href.indexOf('admin.num22.org') >= 0) {
 	// SERVER_PATH2 = 'http://47.52.110.161:7036/'
 	// SERVER_PATH = 'http://47.52.110.161:7036/'
 
-	// SERVER_PATH2 = 'http://c28w749426.zicp.vip/'  //林威远程
-	// SERVER_PATH = 'http://c28w749426.zicp.vip/'
+	SERVER_PATH2 = 'http://c28w749426.zicp.vip/'  //林威远程
+	SERVER_PATH = 'http://c28w749426.zicp.vip/'
 	
 	// SERVER_PATH = 'http://192.168.120.108:7036/'
 	// SERVER_PATH2 = 'http://192.168.120.108:7036/' 
@@ -21,6 +21,6 @@ if(window.location.href.indexOf('admin.num22.org') >= 0) {
 	// SERVER_PATH2 = 'http://192.168.120.42:7036/' 
 	// SERVER_PATH = 'http://192.168.120.53:7036/'  //李海
 	// SERVER_PATH = 'https://api.num22.org/'
-		SERVER_PATH2 = 'https://api.icore.plus/'  //测试
-		SERVER_PATH = 'https://api.icore.plus/'
+		// SERVER_PATH2 = 'https://api.icore.plus/'  //测试
+		// SERVER_PATH = 'https://api.icore.plus/'
 }

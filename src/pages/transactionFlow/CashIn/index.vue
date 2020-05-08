0a5f754840b69c90aea61257fa0f7058a3eaccb3
yilaisai <605333175@qq.com>
2020-05-08 16:12:33 +0800

    增加兑出流水

--- src/pages/transactionFlow/CashIn/index.vue
+++ src/pages/transactionFlow/CashIn/index.vue
@@ -49,6 +49,8 @@
 								<el-select v-model="filterForm.timeOut" >
 									<el-option value="" label="所有"></el-option>
 									<el-option value="1" label="已超时"></el-option>
+									<el-option value="2" label="进行中"></el-option>
+									<el-option value="3" label="已结束"></el-option>
 								</el-select>
 							</el-form-item>
 							<el-button type="primary" @click.native="search" size="mini">搜索</el-button>
@@ -215,7 +217,7 @@ export default {
 				trade_status:'',
 				status: '',
 				tradeType:'3',
-				advType:'',
+				advType:0,
 				dateType:'1',
 				timeOut: "",
 				recdId: "",
@@ -250,11 +252,9 @@ export default {
 				{value:'超时取消',label:"8"}
 			],
 			transList:[
-				{value:'所有',label:""},
-				{value:'抢单兑出',label:"3"},
+				{value:'所有',label:0},
 				{value:'抢单兑入',label:"4"},
 				{value:'派单兑入',label:"5"},
-				{value:'派单兑出',label:"6"},
 			],
 			tradeStatusMap: {
 				1: '未付款',
